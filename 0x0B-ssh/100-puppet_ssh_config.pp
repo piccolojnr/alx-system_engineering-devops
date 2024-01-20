ac2dcf4cb7f47478718f90efde68073b4fc30c36
piccolojnr <rahimdaud246@gmail.com>
2024-01-20 21:01:12 +0000

    cleared typos

--- 0x0B-ssh/100-puppet_ssh_config.pp
+++ 0x0B-ssh/100-puppet_ssh_config.pp
@@ -2,16 +2,15 @@
 include stdlib
 
 file_line { 'Turn off password auth':
-    ensure  => present,
-    path    => '/etc/ssh/ssh_config',
-    line    => '    PasswordAuthentication no',
-    replace =>  true,
+  ensure  => present,
+  path    => '/etc/ssh/ssh_config',
+  line    => 'PasswordAuthentication no',
+  replace => true,
 }
 
-
 file_line { 'Declare file':
-    ensure  => present,
-    path    => '/etc/ssh/ssh_config',
-    line    => '    IdentfyFile ~./ssh/school',
-    replace =>  true,
+  ensure  => present,
+  path    => '/etc/ssh/ssh_config',
+  line    => 'IdentityFile ~/.ssh/school',
+  replace => true,
 }
