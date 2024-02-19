45df0ca5a17cc95bed59ed58cbc9203a1223bed7
piccolojnr <rahimdaud246@gmail.com>
2024-02-19 20:51:54 +0000

    0. Gather data from an API

--- 0x15-api/0-gather_data_from_an_API.py
+++ 0x15-api/0-gather_data_from_an_API.py
@@ -11,27 +11,23 @@ if __name__ == "__main__":
 
     base_url = "https://jsonplaceholder.typicode.com"
     employee_id = int(argv[1])
-    user_url = "{}/users/{}".format(base_url, employee_id)
-    todos_url = "{}/todos?userId={}".format(base_url, employee_id)
 
-    try:
-        user_response = requests.get(user_url)
-        todos_response = requests.get(todos_url)
+    user_response = requests.get(base_url + "/users/{}".format(employee_id))
+    todos_response = requests.get(base_url + "/todos", params={"userId": employee_id})
 
-        user_data = user_response.json()
-        todos_data = todos_response.json()
+    user_data = user_response.json()
+    todos_data = todos_response.json()
 
-        completed_tasks = [task for task in todos_data if task["completed"]]
-        total_tasks = len(todos_data)
+    completed_tasks = [
+        task.get("title") for task in todos_data if task.get("completed") is True
+    ]
+    total_tasks = len(todos_data)
 
-        print(
-            "Employee {} is done with tasks({}/{}):".format(
-                user_data["name"], len(completed_tasks), total_tasks
-            )
+    print(
+        "Employee {} is done with tasks({}/{}):".format(
+            user_data.get("name"), len(completed_tasks), total_tasks
         )
+    )
 
-        for task in completed_tasks:
-            print("\t{}".format(task["title"]))
-
-    except requests.exceptions.RequestException as e:
-        print("Error: {}".format(e))
+    for task in completed_tasks:
+        print("\t{}".format(task))
