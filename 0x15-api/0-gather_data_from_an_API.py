6db35ca0aebd93d0c7704f4df386a9da0215b3ae
piccolojnr <rahimdaud246@gmail.com>
2024-02-19 21:09:53 +0000

    FADE

--- 0x15-api/0-gather_data_from_an_API.py
+++ 0x15-api/0-gather_data_from_an_API.py
@@ -7,10 +7,18 @@ from sys import argv
 if __name__ == "__main__":
     base_url = "https://jsonplaceholder.typicode.com"
     employee_id = int(argv[1])
-    todos_url = "{}/todos?userId={}".format(base_url, employee_id)
+    todos_url = "{}/todos?userId={}".format(
+        base_url,
+        employee_id,
+    )
 
-    user_response = requests.get(base_url + "/users/{}".format(employee_id))
-    todos_response = requests.get(base_url + "/todos", params={"userId": employee_id})
+    user_response = requests.get(
+        base_url + "/users/{}".format(employee_id),
+    )
+    todos_response = requests.get(
+        base_url + "/todos",
+        params={"userId": employee_id},
+    )
 
     user_data = user_response.json()
     todos_data = todos_response.json()
