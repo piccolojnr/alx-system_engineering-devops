e1e0498e529f6efc1f3b08cddaa55f96281076f4
piccolojnr <rahimdaud246@gmail.com>
2024-02-19 21:08:52 +0000

    FDAS

--- 0x15-api/0-gather_data_from_an_API.py
+++ 0x15-api/0-gather_data_from_an_API.py
@@ -5,24 +5,14 @@ import requests
 from sys import argv
 
 if __name__ == "__main__":
-    base_url = "https://jsonplaceholder.typicode.com"
-    employee_id = int(argv[1])
-    todos_url = "{}/todos?userId={}".format(base_url, employee_id)
+    url = "https://jsonplaceholder.typicode.com/"
+    user = requests.get(url + "users/{}".format(argv[1])).json()
+    todos = requests.get(url + "todos", params={"userId": argv[1]}).json()
 
-    user_response = requests.get(base_url + "/users/{}".format(employee_id))
-    todos_response = requests.get(base_url + "/todos", params={"userId": employee_id})
-
-    user_data = user_response.json()
-    todos_data = todos_response.json()
-
-    completed_tasks = [task for task in todos_data if task["completed"]]
-    total_tasks = len(todos_data)
-
+    completed = [t.get("title") for t in todos if t.get("completed") is True]
     print(
         "Employee {} is done with tasks({}/{}):".format(
-            user_data["name"], len(completed_tasks), total_tasks
+            user.get("name"), len(completed), len(todos)
         )
     )
-
-    for task in completed_tasks:
-        print("\t{}".format(task["title"]))
+    [print("\t {}".format(c)) for c in completed]
